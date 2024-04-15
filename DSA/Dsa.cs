a555e70eeb0088db883b704678dc50248323de9b
Bholajie <167084948+Bholajie@users.noreply.github.com>
2024-04-15 13:35:46 +0100

    Add files via upload

---
+++ DSA/Dsa.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+	internal class Dsa
+	{
+
+		public static int[] FindTwoNumbersWithSum2(int[] array, int targetSum)
+		{
+			Dictionary<int, int> numDict = new();
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				int currentNum = array[i];
+				int potentialMatch = targetSum - currentNum;
+
+				if (numDict.ContainsKey(potentialMatch) && numDict[potentialMatch] != i)
+				{
+					return new int[] { potentialMatch, currentNum };
+				}
+
+				if (!numDict.ContainsKey(currentNum))
+				{
+					numDict[currentNum] = i;
+				}
+			}
+			return Array.Empty<int>();
+		}
+
+
+		public static bool IsValidSubsequence(List<int> array, List<int> sequence)
+		{
+			int arrayIndex = 0;
+			int sequenceIndex = 0;
+
+			while (arrayIndex < array.Count && sequenceIndex < sequence.Count)
+			{
+				if (array[arrayIndex] == sequence[sequenceIndex])
+				{
+					sequenceIndex++;
+				}
+				arrayIndex++;
+			}
+			return sequenceIndex == sequence.Count;
+			//return true;
+		}
+
+		public static int[] SortedSquaredArray(int[] nums)
+		{
+			int[] result = new int[nums.Length];
+			int left = 0;
+			int right = nums.Length - 1;
+			int currentIndex = nums.Length - 1;
+
+			while (left <= right)
+			{
+				int leftSquare = nums[left] * nums[left];
+				int rightSquare = nums[right] * nums[right];
+
+				if (leftSquare > rightSquare)
+				{
+					result[currentIndex] = leftSquare;
+					left++;
+				}
+				else
+				{
+					result[currentIndex] = rightSquare;
+					right--;
+				}
+				currentIndex--;
+
+			}
+
+			return result;
+		}
+
+
+		public static string TournamentWinner(List<List<string>> competitions, List<int> results)
+		{
+			if (competitions.Count != results.Count || competitions.Count == 0)
+			{
+				return string.Empty;
+			}
+			var competitionWinner = competitions[0][0];
+			var winnerDict = new Dictionary<string, int> { { competitionWinner, 0 } };
+
+			for (int i = 0; i < competitions.Count; i++)
+			{
+				var roundWinner = competitions[i][1 - results[i]];
+
+				if (!winnerDict.ContainsKey(roundWinner))
+				{
+					winnerDict[roundWinner] = 3;
+				}
+				else
+				{
+					winnerDict[roundWinner] += 3;
+				}
+				if (winnerDict[roundWinner] > winnerDict[competitionWinner])
+				{
+					competitionWinner = roundWinner;
+				}
+			}
+			return competitionWinner;
+		}
+
+		public static int NonConstructibleChange(int[] coins)
+		{
+			Array.Sort(coins);
+			int maxConstructible = 0;
+
+			foreach (int num in coins)
+			{
+				if (num > maxConstructible + 1)
+				{
+
+					return maxConstructible + 1;
+				}
+				maxConstructible += num;
+
+			}
+			return maxConstructible + 1;
+
+		}
+
+		public static int[,] TransposeMatrix(int[,] matrix)
+		{
+			// Write your code here.
+			int rows = matrix.GetLength(0);
+			int columns = matrix.GetLength(1);
+
+			var transposedMatrix = new int[columns, rows];
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int o = 0; o < columns; o++)
+				{
+					transposedMatrix[o, i] = matrix[i, o];
+				}
+			}
+
+			return transposedMatrix;
+		}
+
+		//method to display the matrix
+		public static void DisplayMatrix(int[,] matrix)
+		{
+			int rows = matrix.GetLength(0);
+			int cols = matrix.GetLength(1);
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					Console.Write(matrix[i, j] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+
+		/* public static long FindClosestValueInBst(BST tree, int target)
+         {
+
+             if (tree == null) return int.MaxValue;
+
+             long leftValue = FindClosestValueInBst(tree.left, target);
+             long rightValue = FindClosestValueInBst(tree.right, target);
+
+             long leftClosest = Math.Abs(leftValue - target);
+             long rightClosest = Math.Abs(rightValue - target);
+             long currentClosest = Math.Abs(tree.value - target);
+
+             if (leftClosest < rightClosest)
+             {
+                 return currentClosest < leftClosest ? tree.value : leftValue;
+             }
+             else
+             {
+                 return currentClosest < rightClosest ? tree.value : rightValue;
+             }
+
+         }*/
+
+		public static int FindClosestValueInBst(BST tree, int target)
+		{
+			// Write your code here.
+			var smallestDifference = int.MaxValue;
+			var closestValue = 0;
+			var currentNode = tree;
+			while (currentNode != null)
+			{
+				int currentDifference = Math.Abs(target - currentNode.value);
+
+				if (currentDifference < smallestDifference)
+				{
+					smallestDifference = currentDifference;
+					closestValue = currentNode.value;
+				}
+
+				if (target > currentNode.value)
+				{
+					currentNode = currentNode.right;
+				}
+
+				else
+				{
+					currentNode = currentNode.left;
+				}
+			}
+			return closestValue;
+		}
+
+		public class BST
+		{
+			public int value;
+			public BST left;
+			public BST right;
+
+			public BST(int value)
+			{
+				this.value = value;
+			}
+		}
+
+		public class BinaryTree
+		{
+			public int value;
+			public BinaryTree left;
+			public BinaryTree right;
+
+			public BinaryTree(int value)
+			{
+				this.value = value;
+				this.left = null;
+				this.right = null;
+			}
+		}
+
+		/*public static List<int> BranchSums(BinaryTree root)
+        {
+            // Write your code here.
+            var sums = new List<int>();
+            CalculateBranchSums(root, 0, sums);
+            return sums;
+        }
+
+        public static void CalculateBranchSums(BinaryTree node, int runningSum, List<int> sums)
+        {
+            if (node == null) return;
+
+            var newRunningSum = runningSum + node.value;
+
+            if (node.left == null && node.right == null)
+            {
+                sums.Add(newRunningSum);
+            }
+
+            CalculateBranchSums(node.left, newRunningSum, sums);
+            CalculateBranchSums(node.right, newRunningSum, sums);
+
+        }*/
+
+		public static List<int> BranchSums(BinaryTree root)
+		{
+			var sums = new List<int>();
+			CalculateBranchSums(root, 0, sums);
+			return sums;
+		}
+
+		public static void CalculateBranchSums(BinaryTree node, int runningSum, List<int> sums)
+		{
+			if (node == null) return;
+
+			var newRunningSum = runningSum + node.value;
+
+			if (node.right == null && node.left == null)
+			{
+				sums.Add(newRunningSum);
+			}
+
+			CalculateBranchSums(node.left, newRunningSum, sums);
+			CalculateBranchSums(node.right, newRunningSum, sums);
+		}
+
+		public static int NodeDepths(BinaryTree root)
+		{
+			var sumOfNodeDepths = 0;
+			var stack = new Stack<(BinaryTree node, int nodeDepths)>();
+
+			stack.Push((root, 0));
+
+			while (stack.Count > 0)
+			{
+				var (node, nodeDepths) = stack.Pop();
+				sumOfNodeDepths += nodeDepths;
+
+				if (node.left != null) stack.Push((node.left, nodeDepths + 1));
+				if (node.right != null) stack.Push((node.right, nodeDepths + 1));
+			}
+			return sumOfNodeDepths;
+
+		}
+
+		public static bool ClassPhotos(List<int> redShirtHeights, List<int> blueShirtHeights)
+		{
+			// Write your code here.
+			redShirtHeights.Sort();
+			blueShirtHeights.Sort();
+			var redIsBackRow = redShirtHeights[redShirtHeights.Count - 1] > blueShirtHeights[redShirtHeights.Count - 1];
+			for (int i = 0; i < redShirtHeights.Count; i++)
+			{
+				if (redIsBackRow && redShirtHeights[i] <= blueShirtHeights[i]) return false;
+				else if (!redIsBackRow && blueShirtHeights[i] <= redShirtHeights[i]) return false;
+			}
+			return true;
+		}
+	}
+
+}
---
+++ DSA/Program.cs
@@ -0,0 +1,102 @@
+﻿using DSA;
+using System.Security.Cryptography;
+using static DSA.Dsa;
+
+//Problem 1
+
+/*int[] array = { 3, 5, 5, -4, 8, 11, 1, -1, 6 };
+
+int targetSum = 10;
+
+int[] result = Dsa.FindTwoNumbersWithSum2(array, targetSum);
+if (result.Length == 2)
+{
+    Console.WriteLine($"Two numbers found: {result[0]} and {result[1]}");
+}
+else
+{
+    Console.WriteLine("No two numbers found that sum up to the target sum.");
+}
+
+//Problem 2
+
+List<int> array1 = [1, 2, 4, 6, 7];
+List<int> array2 = [1, 6, 7];
+
+bool result2 = Dsa.IsValidSubsequence(array1, array2);
+Console.WriteLine(result2);*/
+
+//Problem 3
+
+/*int[] input = new int[] { -9, -7, -4, -2, 1, 2, 5, 8 };
+int[] result = Dsa.SortedSquaredArray(input);
+
+Console.WriteLine("Input: [" + string.Join(",", input) + "]");
+Console.WriteLine("Output: [" + string.Join(",", result) + "]");*/
+
+/*List<List<string>> competition = [["a", "b"], ["c", "b"], ["a", "c"]];
+List<int> results = [1, 0, 1];
+
+string totalResult = Dsa.TournamentWinner(competition, results);
+Console.WriteLine(totalResult);*/
+
+/*int[] array = [1, 1,2,3,5,22];
+int result = Dsa.NonConstructibleChange(array);
+
+Console.WriteLine(result);*/
+
+/*int[,] matrix = { { 1,2},
+                    {3,4 },
+                       {5,6 } };
+
+int[,] result = Dsa.TransposeMatrix(matrix);
+//Console.WriteLine(result);
+Dsa.DisplayMatrix(result);*/
+
+// Creating a BST
+/*BST tree = new BST(10);
+tree.left = new BST(8);
+tree.right = new BST(15);
+tree.left.left = new BST(5);
+tree.left.right = new BST(7);
+tree.right.left = new BST(13);
+//tree.right.left.left = new BST(11);
+tree.right.right = new BST(22);
+tree.left.left.left = new BST(1);
+tree.right.left.right = new BST(14);
+
+// Target value
+int targetValue = 6;
+// Calling the method to find the closest value
+long closestValue = Dsa.FindClosestValueInBst(tree, targetValue);
+Console.WriteLine($"Closest value to {targetValue} in the BST: {closestValue}");*/
+
+
+BinaryTree root = new BinaryTree(1)
+{
+    left = new BinaryTree(2)
+    {
+        left = new BinaryTree(4),
+        right = new BinaryTree(5)
+    },
+    right = new BinaryTree(3)
+};
+
+/*var result = Dsa.NodeDepths(root);
+Console.WriteLine(result);*/
+
+// Call the BranchSums method
+List<int> branchSums = BranchSums(root);
+
+// Print the results
+Console.WriteLine("Branch Sums:");
+
+foreach (var sum in branchSums)
+{
+    Console.WriteLine(sum);
+}
+
+List<int> redShirtHeight = [5, 8, 1, 3, 4];
+List<int> blueShirtHeight = [6,9,2,4,5];
+
+Console.WriteLine(ClassPhotos(redShirtHeight, blueShirtHeight));
